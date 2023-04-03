9588500ece3314ba933f21a3a6636503235d27a0
VidhullaSaravana <90876747+VidhullaSaravana@users.noreply.github.com>
2023-04-03 19:22:14 -0400

    Add files via upload

---
+++ Project 117/Video.py
@@ -0,0 +1,32 @@
+import os
+import cv2
+
+path = "Images"
+Images = []
+
+for file in os.listdir(path):
+    name, ext = os.path.splitext(file)
+
+    if ext in ['.gif', '.png', '.jpg', '.jpeg','.jfif']:
+        file_name = path+"/"+file
+
+        print(file_name)
+               
+        Images.append(file_name)
+
+print(len(Images))
+count = len(Images)
+
+frame = cv2.imread(Images[0])
+height, width, channels = frame.shape
+size = (width, height)
+print(size)
+
+out = cv2.VideoWriter('Project.mp4', cv2.VideoWriter_fourcc(*'DIVX'), 5, size)
+
+for i in range(count-1, 0, -1):
+    cv2.imread(Images[i])
+    out.write(frame)
+
+out.release()
+print("Done!")
